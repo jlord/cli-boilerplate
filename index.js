576dfd8cd1351d68ba71930b8ec954cf5431c8f4
Adam Malantonio <adam.malantonio@gmail.com>
2014-09-05 20:17:44 -0400

    absolute path to boilerplate.html
    fixes `{ [Error: ENOENT, open 'boilerplate.html'] errno: 34, code: 'ENOENT', path: 'boilerplate.html' }` error

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 var fs   = require('fs')
+var path = require('path')
 var exec = require('child_process').exec
 
-fs.readFile('boilerplate.html', function read(err, data) {
+fs.readFile(path.join(__dirname, 'boilerplate.html'), function read(err, data) {
   if (err) return console.log(err)
   var command = "echo '" + data.toString() + "' | pbcopy"
   exec(command, function clipboarded(err, stdout, stdrr) {
