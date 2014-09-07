775c2e7899b7c2efe6832edc0d911d9cde701762
Jessica Lord <jessica.lord@github.com>
2014-09-06 21:02:48 -0700

    support more sys

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ var exec = require('child_process').exec
 
 fs.readFile(path.join(__dirname, 'boilerplate.html'), function read(err, data) {
   if (err) return console.log(err)
-  var command = "echo '" + data.toString() + "' | pbcopy"
+  checkSystem(data.toString())
+})
+
+function checkSystem(data) {
+  if (process.platform === "linux") {
+    var command = data + " xclip -selection clipboard"
+    return copy(command)
+  } else if (process.platform === "darwin") {
+    var command = "echo '" + data + "' | pbcopy"
+    return copy(command)
+  } else if (process.platform === "win32") {
+    var command = data + " | pbcopy"
+    return copy(command)
+  } else {
+    return console.log("Sorry your system isn't supported yet :(")
+  }
+}
+
+function copy(command) {
   exec(command, function clipboarded(err, stdout, stdrr) {
     if (err) return console.log(err)
     console.log("Copied!")
   })
-})
+}
