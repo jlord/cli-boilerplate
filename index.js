0fd1af18635a8a2e2186666e45e62a08aafa8d80
Steve Klise <sk@skli.se>
2014-09-10 08:35:21 -0400

    start on file saving

--- index.js
+++ index.js
@@ -3,12 +3,25 @@
 var fs   = require('fs')
 var path = require('path')
 var exec = require('child_process').exec
+var argv = require('minimist')(process.argv.slice(2));
+
 
 fs.readFile(path.join(__dirname, 'boilerplate.html'), function read(err, data) {
   if (err) return console.log(err)
-  var command = "echo '" + data.toString() + "' | pbcopy"
-  exec(command, function clipboarded(err, stdout, stdrr) {
-    if (err) return console.log(err)
-    console.log("Copied!")
-  })
+  var filePath
+  if ((filePath = argv.f) !== undefined) {
+    // Absolute Path
+    if (filePath[0] === "/") {
+
+    // Relative Path
+    } else {
+      path.join(__dirname, filePath)
+    }
+  } else {
+    var command = "echo '" + data.toString() + "' | pbcopy"
+    exec(command, function clipboarded(err, stdout, stdrr) {
+      if (err) return console.log(err)
+      console.log("Copied!")
+    })
+  }
 })
