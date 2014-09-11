009481b07263c18301a0b488d570668499b04fce
Steve Klise <sk@skli.se>
2014-09-10 20:16:21 -0400

    Creates file from boilerplate

--- index.js
+++ index.js
@@ -3,20 +3,19 @@
 var fs   = require('fs')
 var path = require('path')
 var exec = require('child_process').exec
-var argv = require('minimist')(process.argv.slice(2));
-
+var argv = require('minimist')(process.argv.slice(2))
 
 fs.readFile(path.join(__dirname, 'boilerplate.html'), function read(err, data) {
   if (err) return console.log(err)
   var filePath
   if ((filePath = argv.f) !== undefined) {
-    // Absolute Path
-    if (filePath[0] === "/") {
-
-    // Relative Path
-    } else {
-      path.join(__dirname, filePath)
+    if (filePath[0] !== "/") {
+      filePath = path.join(process.cwd(),filePath)
     }
+    fs.writeFile(filePath,data.toString(), function write(err,data) {
+      if (err) return console.log(err)
+      console.log("Boilerplate file created at " + filePath)
+    })
   } else {
     var command = "echo '" + data.toString() + "' | pbcopy"
     exec(command, function clipboarded(err, stdout, stdrr) {
